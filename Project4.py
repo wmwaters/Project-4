60033078a5a767ffd9d84531d682a9f2c35af6e6
William Waters <wmwaters@umich.edu>
2017-12-11 01:39:58 -0500

    started plotly work

--- Project4.py
+++ Project4.py
@@ -7,7 +7,14 @@ import sqlite3
 import requests
 import facebook_info #get personal access token
 access_token = facebook_info.access_token
-import datetime
+import datetime as dt
+import plotly
+plotly.tools.set_credentials_file(username='wmwaters', api_key='ekYBCvTD8xMpGdM8p49r')
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+
+
 
 import sys #prevent unicode errors
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
@@ -28,9 +35,14 @@ except:
 	api_url = "https://graph.facebook.com/v2.11/me/"
 	url_parameters = {}
 	url_parameters["access_token"] = access_token
-	url_parameters["fields"] = "posts.limit(100)"
+	url_parameters["fields"] = "posts.limit(1000)"
 	page = requests.get(api_url, url_parameters)
-	CACHE = page.json()
+	CACHEpre = page.json()
+	CACHE = {"posts" : []}
+	i = 0
+	while (len(CACHE["posts"]) < 100):
+		CACHE["posts"].append(CACHEpre['posts']['data'][i])
+		i += 1
 	cache_file = open(CACHE_FNAME, 'w')
 	json.dump(CACHE, cache_file)
 	cache_file.close()
@@ -39,5 +51,42 @@ conn = sqlite3.connect('project4.sqlite')
 cur = conn.cursor()
 cur.execute('DROP TABLE IF EXISTS Posts')
 cur.execute("CREATE TABLE Posts (id TEXT PRIMARY KEY, posttime TEXT)")
-for post in CACHE['posts']['data']:
-	uprint(post)
+weekdic = {}
+for post in CACHE['posts']:
+	split = post["created_time"].split('T')
+	day = split[0].split('-')
+	time = split[1].split(':')
+	time2 = time[2].split('+')
+	year = int(day[0])
+	month = int(day[1])
+	day = int(day[2])
+	hour = int(time[0])
+	minute = int(time[1])
+	second = int(time2[0])
+	daytimeobj = dt.datetime(year, month, day, hour, minute, second)
+	weekda = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+	if weekda[daytimeobj.weekday()] in weekdic:
+		weekdic[weekda[daytimeobj.weekday()]]+=1
+	else:
+		weekdic[weekda[daytimeobj.weekday()]]=1
+
+	tp = (post["id"], daytimeobj)
+	cur.execute("INSERT INTO Posts (id, posttime) VALUES (?,?)", tp)
+
+
+conn.commit()
+conn.close()
+day_values = []
+day_values.append(weekdic["Monday"])
+day_values.append(weekdic["Tuesday"])
+day_values.append(weekdic["Wednesday"])
+day_values.append(weekdic["Thursday"])
+day_values.append(weekdic["Friday"])
+day_values.append(weekdic["Saturday"])
+day_values.append(weekdic["Sunday"])
+print(day_values)
+data = [go.Bar(
+            x=['giraffes', 'orangutans', 'monkeys'],
+            y=[20, 14, 23])]
+#data = [go.Bar(x = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], y = day_values)]
+py.plot(data, filename='basic-bar')
