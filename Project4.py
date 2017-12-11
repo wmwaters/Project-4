8d4d375a6a6c42f2d74676ba94adbe68059e714f
William Waters <wmwaters@umich.edu>
2017-12-10 23:46:13 -0500

    database work

--- Project4.py
+++ Project4.py
@@ -5,7 +5,9 @@
 import json
 import sqlite3
 import requests
-import facebook_info.py #get personal access token
+import facebook_info #get personal access token
+access_token = facebook_info.access_token
+import datetime
 
 import sys #prevent unicode errors
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
@@ -18,11 +20,24 @@ def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
 
 CACHE_FNAME = "206_Project4_cache.json"
 try:
-    cache_file = open(CACHE_FNAME,'r')
+    cache_file = open(CACHE_FNAME, 'r')
     cache_contents = cache_file.read()
     cache_file.close()
-    CACHE_DICTION = json.loads(cache_contents)
+    CACHE = json.loads(cache_contents)
 except:
-    r = requests.get("https://graph.facebook.com/v2.11/me/",params={"limit":2, "access_token":access_token})
+	api_url = "https://graph.facebook.com/v2.11/me/"
+	url_parameters = {}
+	url_parameters["access_token"] = access_token
+	url_parameters["fields"] = "posts.limit(100)"
+	page = requests.get(api_url, url_parameters)
+	CACHE = page.json()
+	cache_file = open(CACHE_FNAME, 'w')
+	json.dump(CACHE, cache_file)
+	cache_file.close()
 
-
+conn = sqlite3.connect('project4.sqlite')
+cur = conn.cursor()
+cur.execute('DROP TABLE IF EXISTS Posts')
+cur.execute("CREATE TABLE Posts (id TEXT PRIMARY KEY, posttime TEXT)")
+for post in CACHE['posts']['data']:
+	uprint(post)
