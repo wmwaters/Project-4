c472b1280bf65d3b7b65be14765160143ca01cd3
William Waters <wmwaters@umich.edu>
2017-12-11 13:39:28 -0500

    final project done and commented

--- Project4.py
+++ Project4.py
@@ -5,16 +5,15 @@
 import json
 import sqlite3
 import requests
-import facebook_info #get personal access token
-access_token = facebook_info.access_token
 import datetime as dt
+
+import facebook_info #get personal access token file
+access_token = facebook_info.access_token #getting facebook access token from file in directory
+
 import plotly
-plotly.tools.set_credentials_file(username='wmwaters', api_key='ekYBCvTD8xMpGdM8p49r')
+plotly.tools.set_credentials_file(username = 'wmwaters', api_key = 'ekYBCvTD8xMpGdM8p49r') #setting api access code for ploy.ly
 import plotly.plotly as py
 import plotly.graph_objs as go
-
-
-
 
 import sys #prevent unicode errors
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
@@ -26,19 +25,19 @@ def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
         print(*map(f, objects), sep=sep, end=end, file=file)
 
 CACHE_FNAME = "206_Project4_cache.json"
-try:
+try: #opening cache file if it exists
     cache_file = open(CACHE_FNAME, 'r')
     cache_contents = cache_file.read()
     cache_file.close()
     CACHE = json.loads(cache_contents)
-except:
+except: #creating cache file from pulled data if there isn't already a cache
 	api_url = "https://graph.facebook.com/v2.11/me/"
-	url_parameters = {}
+	url_parameters = {} #building url to get 1000 posts from Facebook
 	url_parameters["access_token"] = access_token
 	url_parameters["fields"] = "posts.limit(1000)"
-	page = requests.get(api_url, url_parameters)
+	page = requests.get(api_url, url_parameters) #requested Facebook data
 	CACHEpre = page.json()
-	CACHE = {"posts" : []}
+	CACHE = {"posts" : []} #got only 73 back when pulling 100 posts, so this following loop limits from 1000 items to 100 posts
 	i = 0
 	while (len(CACHE["posts"]) < 100):
 		CACHE["posts"].append(CACHEpre['posts']['data'][i])
@@ -47,12 +46,12 @@ except:
 	json.dump(CACHE, cache_file)
 	cache_file.close()
 
-conn = sqlite3.connect('project4.sqlite')
+conn = sqlite3.connect('project4.sqlite') #creating or opening database
 cur = conn.cursor()
-cur.execute('DROP TABLE IF EXISTS Posts')
-cur.execute("CREATE TABLE Posts (id TEXT PRIMARY KEY, posttime TEXT)")
+cur.execute('DROP TABLE IF EXISTS Posts') #deleting table if it already exists
+cur.execute("CREATE TABLE Posts (id TEXT PRIMARY KEY, posttime TEXT)") #setting items in database
 weekdic = {}
-for post in CACHE['posts']:
+for post in CACHE['posts']: #creating datetime object for each post
 	split = post["created_time"].split('T')
 	day = split[0].split('-')
 	time = split[1].split(':')
@@ -64,19 +63,20 @@ for post in CACHE['posts']:
 	minute = int(time[1])
 	second = int(time2[0])
 	daytimeobj = dt.datetime(year, month, day, hour, minute, second)
-	weekda = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-	if weekda[daytimeobj.weekday()] in weekdic:
-		weekdic[weekda[daytimeobj.weekday()]]+=1
+	weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+	if weekdays[daytimeobj.weekday()] in weekdic: #counting the number of posts on each day
+		weekdic[weekdays[daytimeobj.weekday()]] += 1
 	else:
-		weekdic[weekda[daytimeobj.weekday()]]=1
+		weekdic[weekdays[daytimeobj.weekday()]] = 1
 
-	tp = (post["id"], daytimeobj)
-	cur.execute("INSERT INTO Posts (id, posttime) VALUES (?,?)", tp)
+	tp = (post["id"], daytimeobj) #creating tuple to insert into database
+	cur.execute("INSERT INTO Posts (id, posttime) VALUES (?,?)", tp) #putting item into database
 
 
 conn.commit()
 conn.close()
-day_values = []
+
+day_values = [] #creating x-axis values list
 day_values.append(weekdic["Monday"])
 day_values.append(weekdic["Tuesday"])
 day_values.append(weekdic["Wednesday"])
@@ -84,9 +84,12 @@ day_values.append(weekdic["Thursday"])
 day_values.append(weekdic["Friday"])
 day_values.append(weekdic["Saturday"])
 day_values.append(weekdic["Sunday"])
-print(day_values)
-data = [go.Bar(
-            x=['giraffes', 'orangutans', 'monkeys'],
-            y=[20, 14, 23])]
-#data = [go.Bar(x = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], y = day_values)]
-py.plot(data, filename='basic-bar')
+#creating x-y data for plotly as trace0
+trace0 = go.Bar(x = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], y = day_values, name = 'Name of Trace 0')
+data = [trace0] #turning trace0 into a list
+#making the graph have a title and axis labels
+layout = go.Layout(title = 'Facebook Posts By Day of Week', xaxis = dict(title = 'Days of Week', titlefont = dict(family = 'Courier New, monospace', size = 18, color = '#7f7f7f')), yaxis = dict(title = 'Number of Facebook Posts', titlefont = dict(family = 'Courier New, monospace', size = 18, color = '#7f7f7f')))
+#creating a figure from the combined data and layout objects
+fig = go.Figure(data = data, layout = layout)
+#sending my graph to plot.ly
+py.plot(fig, filename = '206 Final Project Facebook Post Graph')
